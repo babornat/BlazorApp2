8765a3a58c793e76e1c97402c2ae6e37fddcdd64
babornat <natya099@gmail.com>
2022-03-12 23:29:23 +0100

    ukol4

---
+++ Ukol4/PptNemocnice.Shared/vybaveniModel.cs
@@ -0,0 +1,20 @@
+﻿namespace PptNemocnice.Shared
+{
+    public class vybaveniModel
+    {
+        public string Name { get; set; }
+        public DateTime BoughtDate { get; set; }
+        public DateTime LastRevisionDate { get; set; }
+        public bool NeedRevision { get; }
+
+        public bool IsInEditMode { get; set; }
+
+        public vybaveniModel(string name, DateTime bought, DateTime revision, bool needRevision)
+        {
+            Name = name;
+            BoughtDate = bought;
+            LastRevisionDate = revision;
+            NeedRevision = needRevision;
+        }
+    }
+}
---
+++ Ukol4/PptNemocnice/VybaveniModel.cs
@@ -0,0 +1,20 @@
+﻿namespace PptNemocnice
+{ 
+    public class VybaveniModel
+    {
+
+        public string Name { get; set; }
+        public DateTime BoughtDate { get; set; }
+        public DateTime LastRevisionDate { get; set; }
+        public bool NeedRevision { get; }
+
+        public VybaveniModel(string name, DateTime bought, DateTime revision, bool needRevision)
+        {
+            Name = name;
+            BoughtDate = bought;
+            LastRevisionDate = revision;
+            NeedRevision = needRevision;
+        }
+
+    }
+}
