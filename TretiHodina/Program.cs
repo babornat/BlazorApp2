68db6853953b0337ec531a2d4697971fffc3a9a4
babornat <natya099@gmail.com>
2022-03-12 23:50:33 +0100

    commit po rozbiti

--- TretiHodina/Program.cs
+++ TretiHodina/Program.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using TretiHodina;
+using System.Linq;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
